ac027a9b076c7c1fd6ab49b34ce71346c29018dd
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2020-11-12 18:18:18 +0100

    Augment only training data

--- 1-img_classification/homebrew_model.py
+++ 1-img_classification/homebrew_model.py
@@ -39,15 +39,100 @@ def homebrew_model(img_h, img_w, num_classes):
             padding='same',
             input_shape=input_shape
         ))
+        model.add(tf.keras.layers.BatchNormalization())
         model.add(tf.keras.layers.ReLU())
         model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
         start_f *= 2
         
     # Classifier
-    # model.add(tf.keras.layers.GlobalAveragePooling2D())
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(units=512, activation='relu'))
     model.add(tf.keras.layers.Dense(units=num_classes, activation='softmax'))
+
+    return model
+
+def set_seeds(seed):
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+if __name__ == "__main__":
+    AUGMENT_DATA = True # @Note Worse if augmenting
+    CHECKPOINTS = False
+    EARLY_STOP = True
+    TENSORBOARD = False
+    SAVE_BEST = True
+    VALIDATION_SPLIT = 0.2
+
+    # Set global seed for all internal generators, this should make all randomization reproducible
+    import signal
+    SEED = signal.SIGSEGV.value
+    set_seeds(SEED)
+
+    # Data generator
+    # Split training and validation data automatically
+    if AUGMENT_DATA:
+        train_gen = ImageDataGenerator(
+            rotation_range=10,
+            width_shift_range=10,
+            height_shift_range=10,
+            zoom_range=0.3,
+            horizontal_flip=True,
+            vertical_flip=True,
+            fill_mode='constant',
+            cval=0,
+            rescale=1./255,
+            validation_split=VALIDATION_SPLIT,
+        )
+    else:
+        train_gen = ImageDataGenerator(
+            rescale=1./255,
+            validation_split=VALIDATION_SPLIT, 
+        )
+
+    valid_gen = ImageDataGenerator(
+        rescale=1./255,
+        validation_split=VALIDATION_SPLIT, 
+    )
+
+    # Training and validation datasets
+    dataset_dir = "MaskDataset"
+
+    bs = 8
+    img_h = 256
+    img_w = 256
+
+    num_classes = len(classes)
+
+    training_dir = f"{dataset_dir}/training-structured"
+
+    def flow(generator, subset):
+        return generator.flow_from_directory(
+            training_dir,
+            target_size=(img_h, img_w),
+            color_mode='rgb',
+            batch_size=bs,
+            classes=classes,
+            class_mode='categorical',
+            shuffle=True,
+            seed=SEED, # Ensure it always shuffles with the same seed so the splits are correct
+            subset=subset,
+        )
+
+    train_flow = flow(train_gen, 'training')
+    validation_flow = flow(valid_gen, 'validation')
+
+    def dataset_from_flow(flow):
+        return tf.data.Dataset.from_generator(
+            lambda: flow,
+            output_types=(tf.float32, tf.float32),
+            output_shapes=([None, img_h, img_w, 3], [None, num_classes])
+        ).repeat()
+
+    train_dataset = dataset_from_flow(train_flow)
+    validation_dataset = dataset_from_flow(validation_flow)
+
+    model = homebrew_model(img_h, img_w, num_classes)
 
     # Optimization params
     # -------------------
@@ -68,83 +153,6 @@ def homebrew_model(img_h, img_w, num_classes):
 
     # Compile Model
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-    return model
-
-def set_seeds(seed):
-    tf.random.set_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-
-if __name__ == "__main__":
-    AUGMENT_DATA = True # @Note Worse if augmenting
-    CHECKPOINTS = False
-    EARLY_STOP = True
-    TENSORBOARD = False
-    SAVE_BEST = True
-	
-    # Set global seed for all internal generators, this should make all randomization reproducible
-    import signal
-    SEED = signal.SIGSEGV.value
-    set_seeds(SEED)
-
-    # Data generator
-    # Single image generator, splitting the entire dataset in training and validation
-    if AUGMENT_DATA:
-        data_gen = ImageDataGenerator(
-            # rotation_range=10,
-            width_shift_range=10,
-            height_shift_range=10,
-            zoom_range=0.3,
-            horizontal_flip=True,
-            vertical_flip=True,
-            fill_mode='constant',
-            cval=0,
-            rescale=1./255, 
-            validation_split=0.2
-        )
-    else:
-        data_gen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-
-    # Training and validation datasets
-    dataset_dir = "MaskDataset"
-
-    bs = 8
-    img_h = 256
-    img_w = 256
-
-    num_classes = len(classes)
-
-    training_dir = f"{dataset_dir}/training-structured"
-
-    def flows(generator):
-        def build_flow(subset):
-            return generator.flow_from_directory(
-                training_dir,
-                target_size=(img_h, img_w),
-                color_mode='rgb',
-                batch_size=bs,
-                classes=classes,
-                class_mode='categorical',
-                shuffle=True,
-                subset=subset,
-            )
-
-        return build_flow('training'), build_flow('validation')
-
-    train_gen, validation_gen = flows(data_gen)
-
-    def dataset_from_flow(flow):
-        return tf.data.Dataset.from_generator(
-            lambda: flow,
-            output_types=(tf.float32, tf.float32),
-            output_shapes=([None, img_h, img_w, 3], [None, num_classes])
-        ).repeat()
-
-    train_dataset = dataset_from_flow(train_gen)
-    validation_dataset = dataset_from_flow(validation_gen)
-
-    model = homebrew_model(img_h, img_w, num_classes)
 
     callbacks_list = []
 
@@ -187,23 +195,8 @@ if __name__ == "__main__":
     model.fit(
         x=train_dataset,
         epochs=100,
-        steps_per_epoch=len(train_gen),
+        steps_per_epoch=len(train_flow),
         validation_data=validation_dataset,
-        validation_steps=len(validation_gen),
+        validation_steps=len(validation_flow),
         callbacks=callbacks_list,
     )
-
-    """
-    Best so far (see best.txt for full logs)
-
-    Epoch 1/100
-    562/562 [==============================] - 24s 43ms/step - loss: 1.0989 - accuracy: 0.3451 - val_loss: 1.0992 - val_accuracy: 0.3396
-    Epoch 2/100
-    562/562 [==============================] - 23s 42ms/step - loss: 1.0965 - accuracy: 0.3631 - val_loss: 1.0948 - val_accuracy: 0.3636
-
-    Epoch 17/100
-    562/562 [==============================] - 23s 41ms/step - loss: 0.3627 - accuracy: 0.8493 - val_loss: 0.7742 - val_accuracy: 0.6684
-    Epoch 18/100
-    562/562 [==============================] - 23s 41ms/step - loss: 0.3241 - accuracy: 0.8651 - val_loss: 0.7710 - val_accuracy: 0.6622
-
-    """
--- 1-img_classification/transfer_model.py
+++ 1-img_classification/transfer_model.py
@@ -11,20 +11,20 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # local imports
 from classes import classes
 import callbacks
-from csv_generator import create_csv
 
 MODEL_NAME = 'MaskDetection-Transfer'
 
-def VGG_transfer_model(img_h, img_w, num_classes, finetuning=False):
+def VGG_transfer_model(img_h, img_w, num_classes, train_mode=False):
     vgg = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(img_h, img_w, 3))
 
-    if finetuning:
+    if train_mode:
+        if FINETUNING:
             freeze_until = 15
 
             for layer in vgg.layers[:freeze_until]:
                 layer.trainable = False
-    else:
-        vgg.trainable = False
+        else:
+            vgg.trainable = False
 
     model = tf.keras.Sequential()
     model.add(vgg)
@@ -32,13 +32,6 @@ def VGG_transfer_model(img_h, img_w, num_classes, finetuning=False):
     model.add(tf.keras.layers.Dense(units=512, activation='relu'))
     model.add(tf.keras.layers.Dense(units=num_classes, activation='softmax'))
 
-    loss = tf.keras.losses.CategoricalCrossentropy()
-    lr = 1e-4
-    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-    metrics = ['accuracy']
-
-    # Compile Model
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
     return model
 
 def set_seeds(seed):
@@ -52,6 +45,8 @@ if __name__ == "__main__":
     EARLY_STOP = True
     FINETUNING = True
     SAVE_BEST = True
+    VALIDATION_SPLIT = 0.2
+    LEARNING_RATE = 1e-4
 
     img_h = 256
     img_w = 256
@@ -64,10 +59,10 @@ if __name__ == "__main__":
     preprocess_input = tf.keras.applications.vgg16.preprocess_input
 
     # Data generator
-    # Single image generator, splitting the entire dataset in training and validation
+    # Split training and validation data automatically
     if AUGMENT_DATA:
-        data_gen = ImageDataGenerator(
-            # rotation_range=10,
+        train_gen = ImageDataGenerator(
+            rotation_range=10,
             width_shift_range=10,
             height_shift_range=10,
             zoom_range=0.3,
@@ -76,15 +71,21 @@ if __name__ == "__main__":
             fill_mode='constant',
             cval=0,
             rescale=1./255,
-            validation_split=0.2,
+            validation_split=VALIDATION_SPLIT,
             preprocessing_function=preprocess_input,
         )
     else:
-        data_gen = ImageDataGenerator(
+        train_gen = ImageDataGenerator(
             rescale=1./255,
-            validation_split=0.2, 
+            validation_split=VALIDATION_SPLIT, 
             preprocessing_function=preprocess_input,
         )
+
+    valid_gen = ImageDataGenerator(
+        rescale=1./255,
+        validation_split=VALIDATION_SPLIT, 
+        preprocessing_function=preprocess_input,
+    )
 
     # Training and validation datasets
     dataset_dir = "MaskDataset"
@@ -97,22 +98,20 @@ if __name__ == "__main__":
 
     training_dir = f"{dataset_dir}/training-structured"
 
-    def flows(generator):
-        def build_flow(subset):
-            return generator.flow_from_directory(
-                training_dir,
-                target_size=(img_h, img_w),
-                color_mode='rgb',
-                batch_size=bs,
-                classes=classes,
-                class_mode='categorical',
-                shuffle=True,
-                subset=subset,
-            )
+    def flow(generator, subset):
+        return generator.flow_from_directory(
+            training_dir,
+            target_size=(img_h, img_w),
+            color_mode='rgb',
+            batch_size=bs,
+            classes=classes,
+            class_mode='categorical',
+            shuffle=True,
+            subset=subset,
+        )
 
-        return build_flow('training'), build_flow('validation')
-
-    train_gen, validation_gen = flows(data_gen)
+    train_flow = flow(train_gen, 'training')
+    validation_flow = flow(valid_gen, 'validation')
 
     def dataset_from_flow(flow):
         return tf.data.Dataset.from_generator(
@@ -121,12 +120,19 @@ if __name__ == "__main__":
             output_shapes=([None, img_h, img_w, 3], [None, num_classes])
         ).repeat()
 
-    train_dataset = dataset_from_flow(train_gen)
-    validation_dataset = dataset_from_flow(validation_gen)
+    train_dataset = dataset_from_flow(train_flow)
+    validation_dataset = dataset_from_flow(validation_flow)
 
     # --------- Training ---------
 
-    model = VGG_transfer_model(img_h, img_w, num_classes, FINETUNING)
+    model = VGG_transfer_model(img_h, img_w, num_classes, train_mode=True)
+
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+    metrics = ['accuracy']
+
+    # Compile Model
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     callbacks_list = []
 
@@ -161,27 +167,8 @@ if __name__ == "__main__":
 
     model.fit(x=train_dataset,
         epochs=100,
-        steps_per_epoch=len(train_gen),
+        steps_per_epoch=len(train_flow),
         validation_data=validation_dataset,
-        validation_steps=len(validation_gen),
+        validation_steps=len(validation_flow),
         callbacks=callbacks_list,
     )
-
-
-    '''
-    562/562 [==============================] - 48s 86ms/step - loss: 0.7169 - accuracy: 0.6569 - val_loss: 0.4852 - val_accuracy: 0.7736
-    Epoch 2/100
-    562/562 [==============================] - 47s 84ms/step - loss: 0.4637 - accuracy: 0.7963 - val_loss: 0.4945 - val_accuracy: 0.7790
-    Epoch 3/100
-    562/562 [==============================] - 48s 86ms/step - loss: 0.3469 - accuracy: 0.8544 - val_loss: 0.4232 - val_accuracy: 0.8137
-    Epoch 4/100
-    562/562 [==============================] - 49s 86ms/step - loss: 0.2461 - accuracy: 0.9025 - val_loss: 0.4587 - val_accuracy: 0.8191
-    Epoch 5/100
-    562/562 [==============================] - 49s 87ms/step - loss: 0.1542 - accuracy: 0.9450 - val_loss: 0.5308 - val_accuracy: 0.8209
-    Epoch 6/100
-    562/562 [==============================] - 49s 86ms/step - loss: 0.0935 - accuracy: 0.9659 - val_loss: 0.5706 - val_accuracy: 0.8191
-    Epoch 7/100
-    562/562 [==============================] - 48s 86ms/step - loss: 0.0706 - accuracy: 0.9744 - val_loss: 0.6243 - val_accuracy: 0.8324
-    Epoch 8/100
-    562/562 [==============================] - 49s 87ms/step - loss: 0.0329 - accuracy: 0.9907 - val_loss: 0.6994 - val_accuracy: 0.8316
-    '''
