0391202806d7918098244d7ed28bd3e5528b7a75
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2020-12-17 18:50:46 +0100

    Fix indentation

--- 2-img_segmentation/vgg_base.py
+++ 2-img_segmentation/vgg_base.py
@@ -20,99 +20,100 @@ from metrics import gen_meanIoU
 MODEL_NAME = 'Segmentation-transfer'
 
 class CustomDataset(tf.keras.utils.Sequence):
-  """
-    CustomDataset inheriting from tf.keras.utils.Sequence.
+    """
+        CustomDataset inheriting from tf.keras.utils.Sequence.
 
-    3 main methods:
-      - __init__: save dataset params like directory, filenames..
-      - __len__: return the total number of samples in the dataset
-      - __getitem__: return a sample from the dataset
+        3 main methods:
+        - __init__: save dataset params like directory, filenames..
+        - __len__: return the total number of samples in the dataset
+        - __getitem__: return a sample from the dataset
 
-    Note: 
-      - the custom dataset returns a single sample from the dataset. Then, we use 
-        a tf.data.Dataset object to group samples into batches.
-      - in this case we have a different structure of the dataset in memory. 
-        We have all the images in the same folder and the training and validation splits
-        are defined in text files.
-  """
+        Note: 
+        - the custom dataset returns a single sample from the dataset. Then, we use 
+            a tf.data.Dataset object to group samples into batches.
+        - in this case we have a different structure of the dataset in memory. 
+            We have all the images in the same folder and the training and validation splits
+            are defined in text files.
+    """
 
-  def __init__(self, dataset_dir, which_subset, img_generator=None, mask_generator=None, 
-               preprocessing_function=None, out_shape=[256, 256]):
-    if which_subset == 'training':
-        subset_file = os.path.join(dataset_dir, 'Splits', 'train.txt')
-    elif which_subset == 'validation':
-        subset_file = os.path.join(dataset_dir, 'Splits', 'val.txt')
-    
-    with open(subset_file, 'r') as f:
-        lines = f.readlines()
-    
-    subset_filenames = []
-    for line in lines:
-        subset_filenames.append(line.strip()) 
+    def __init__(self, dataset_dir, which_subset, img_generator=None, mask_generator=None, 
+        preprocessing_function=None, out_shape=[256, 256]):
 
-    self.which_subset = which_subset
-    self.dataset_dir = dataset_dir
-    self.subset_filenames = subset_filenames
-    self.img_generator = img_generator
-    self.mask_generator = mask_generator
-    self.preprocessing_function = preprocessing_function
-    self.out_shape = out_shape
+        if which_subset == 'training':
+            subset_file = os.path.join(dataset_dir, 'Splits', 'train.txt')
+        elif which_subset == 'validation':
+            subset_file = os.path.join(dataset_dir, 'Splits', 'val.txt')
+        
+        with open(subset_file, 'r') as f:
+            lines = f.readlines()
+        
+        subset_filenames = []
+        for line in lines:
+            subset_filenames.append(line.strip()) 
 
-  def __len__(self):
-    return len(self.subset_filenames)
+        self.which_subset = which_subset
+        self.dataset_dir = dataset_dir
+        self.subset_filenames = subset_filenames
+        self.img_generator = img_generator
+        self.mask_generator = mask_generator
+        self.preprocessing_function = preprocessing_function
+        self.out_shape = out_shape
 
-  def __getitem__(self, index):
-    # Read Image
-    curr_filename = self.subset_filenames[index]
-    img = Image.open(os.path.join(self.dataset_dir, 'Images', curr_filename + '.jpg'))
-    mask = Image.open(os.path.join(self.dataset_dir, 'Masks', curr_filename + '.png'))
-    
-    # Resize image and mask
-    img = img.resize(self.out_shape)
-    mask = mask.resize(self.out_shape, resample=Image.NEAREST)
-    
-    img_arr = np.array(img)
-    mask_arr = np.array(mask)
+    def __len__(self):
+        return len(self.subset_filenames)
 
-    # Convert RGB mask for each class to numbers from 0 to 2
-    new_mask_arr = np.zeros(mask_arr.shape[:2], dtype=mask_arr.dtype)
-    new_mask_arr = np.expand_dims(new_mask_arr, -1)
+    def __getitem__(self, index):
+        # Read Image
+        curr_filename = self.subset_filenames[index]
+        img = Image.open(os.path.join(self.dataset_dir, 'Images', curr_filename + '.jpg'))
+        mask = Image.open(os.path.join(self.dataset_dir, 'Masks', curr_filename + '.png'))
+        
+        # Resize image and mask
+        img = img.resize(self.out_shape)
+        mask = mask.resize(self.out_shape, resample=Image.NEAREST)
+        
+        img_arr = np.array(img)
+        mask_arr = np.array(mask)
 
-    new_mask_arr[np.where(np.all(mask_arr == BACKGROUND_0, axis=-1))] = 0
-    new_mask_arr[np.where(np.all(mask_arr == BACKGROUND_1, axis=-1))] = 0
-    new_mask_arr[np.where(np.all(mask_arr == CROP, axis=-1))] = 1
-    new_mask_arr[np.where(np.all(mask_arr == WEED, axis=-1))] = 2
+        # Convert RGB mask for each class to numbers from 0 to 2
+        new_mask_arr = np.zeros(mask_arr.shape[:2], dtype=mask_arr.dtype)
+        new_mask_arr = np.expand_dims(new_mask_arr, -1)
 
-    if self.which_subset == 'training':
-      if self.img_generator is not None and self.mask_generator is not None:
-        # Perform data augmentation
-        # We can get a random transformation from the ImageDataGenerator using get_random_transform
-        # and we can apply it to the image using apply_transform
-        img_t = self.img_generator.get_random_transform(img_arr.shape, seed=SEED)
-        mask_t = self.mask_generator.get_random_transform(mask_arr.shape, seed=SEED)
-        img_arr = self.img_generator.apply_transform(img_arr, img_t)
-        # ImageDataGenerator use bilinear interpolation for augmenting the images.
-        # Thus, when applied to the masks it will output 'interpolated classes', which
-        # is an unwanted behaviour. As a trick, we can transform each class mask 
-        # separately and then we can cast to integer values (as in the binary segmentation notebook).
-        # Finally, we merge the augmented binary masks to obtain the final segmentation mask.
-        out_mask = np.zeros_like(new_mask_arr)
-        for c in np.unique(new_mask_arr):
-          if c > 0:
-            curr_class_arr = np.float32(new_mask_arr == c)
-            curr_class_arr = self.mask_generator.apply_transform(curr_class_arr, mask_t)
-            # from [0, 1] to {0, 1}
-            curr_class_arr = np.uint8(curr_class_arr)
-            # recover original class
-            curr_class_arr = curr_class_arr * c 
-            out_mask += curr_class_arr
-    else:
-      out_mask = new_mask_arr
-    
-    if self.preprocessing_function is not None:
-        img_arr = self.preprocessing_function(img_arr)
+        new_mask_arr[np.where(np.all(mask_arr == BACKGROUND_0, axis=-1))] = 0
+        new_mask_arr[np.where(np.all(mask_arr == BACKGROUND_1, axis=-1))] = 0
+        new_mask_arr[np.where(np.all(mask_arr == CROP, axis=-1))] = 1
+        new_mask_arr[np.where(np.all(mask_arr == WEED, axis=-1))] = 2
 
-    return img_arr, np.float32(out_mask)
+        if self.which_subset == 'training':
+            if self.img_generator is not None and self.mask_generator is not None:
+                # Perform data augmentation
+                # We can get a random transformation from the ImageDataGenerator using get_random_transform
+                # and we can apply it to the image using apply_transform
+                img_t = self.img_generator.get_random_transform(img_arr.shape, seed=SEED)
+                mask_t = self.mask_generator.get_random_transform(mask_arr.shape, seed=SEED)
+                img_arr = self.img_generator.apply_transform(img_arr, img_t)
+                # ImageDataGenerator use bilinear interpolation for augmenting the images.
+                # Thus, when applied to the masks it will output 'interpolated classes', which
+                # is an unwanted behaviour. As a trick, we can transform each class mask 
+                # separately and then we can cast to integer values (as in the binary segmentation notebook).
+                # Finally, we merge the augmented binary masks to obtain the final segmentation mask.
+                out_mask = np.zeros_like(new_mask_arr)
+                for c in np.unique(new_mask_arr):
+                    if c > 0:
+                        curr_class_arr = np.float32(new_mask_arr == c)
+                        curr_class_arr = self.mask_generator.apply_transform(curr_class_arr, mask_t)
+                        # from [0, 1] to {0, 1}
+                        curr_class_arr = np.uint8(curr_class_arr)
+                        # recover original class
+                        curr_class_arr = curr_class_arr * c 
+                        out_mask += curr_class_arr
+        else:
+            out_mask = new_mask_arr
+        
+        if self.preprocessing_function is not None:
+            img_arr = self.preprocessing_function(img_arr)
+
+        return img_arr, np.float32(out_mask)
 
 # ---- Model ----
 def create_model(depth, num_classes):
@@ -165,13 +166,13 @@ if __name__ == "__main__":
     CHECKPOINTS = False
     SAVE_BEST = True
     EARLY_STOP = True
-    TRAIN_ALL = True
+    TRAIN_ALL = False
 
     if TRAIN_ALL:
         PLOT = False
     else: 
-        subdataset = Subdataset.BIPBIP.value
-        species = Species.HARICOT.value 
+        subdataset = Subdataset.BIPBIP
+        species = Species.HARICOT
         PLOT = True
 
     img_h = 256
